1b3dc36e7e209be6fbef75ab1579e2752bfa9e92
Loïck Piera <pyrech@gmail.com>
2023-05-15 20:18:51 +0200

    Allow to configure env vars

---
+++ example/env.runit.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Asrunit\Example;
+
+use Asrunit\Attribute\AsContext;
+use Asrunit\Attribute\Task;
+use Asrunit\Context;
+use function Asrunit\exec;
+
+#[AsContext(name: 'context_env')]
+function context_env(): Context {
+    return new Context(environment: [
+        'FOO' => 'toto',
+    ]);
+}
+
+#[Task(description: "A simple task that use environment variables")]
+function env(Context $context) {
+    exec('echo $FOO $BAR', environment: ['BAR' => 'tata']);
+}
